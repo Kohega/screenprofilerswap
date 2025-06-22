65083bab5e9e540ecc86cd748212c50dbfda8c0f
Kohega <45211407+Kohega@users.noreply.github.com>
2025-06-22 16:10:33 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Kohega/screenprofilerswap.git
+cd screenprofilerswap
+chmod +x screenprofilerswap
+chmod +x screenprofilerback
+ln -s $PWD/screenprofilerswap ~/.local/bin/screenprofilerswap
+ln -s $PWD/screenprofilerback ~/.local/bin/screenprofilerback
